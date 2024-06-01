0c493ec64d597e66024c632e0803021c642731cf
vijay <g.vijay946@gamil.com>
2024-06-01 13:27:05 +0530

    initial commit

---
+++ src/components/Navbars/ExamplesNavbar.js
@@ -0,0 +1,140 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { Link } from "react-router-dom";
+// nodejs library that concatenates strings
+import classnames from "classnames";
+
+// reactstrap components
+import {
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Button,
+} from "reactstrap";
+
+function ExamplesNavbar() {
+  const [navbarColor, setNavbarColor] = React.useState("navbar-transparent");
+  const [navbarCollapse, setNavbarCollapse] = React.useState(false);
+
+  const toggleNavbarCollapse = () => {
+    setNavbarCollapse(!navbarCollapse);
+    document.documentElement.classList.toggle("nav-open");
+  };
+
+  React.useEffect(() => {
+    const updateNavbarColor = () => {
+      if (
+        document.documentElement.scrollTop > 299 ||
+        document.body.scrollTop > 299
+      ) {
+        setNavbarColor("");
+      } else if (
+        document.documentElement.scrollTop < 300 ||
+        document.body.scrollTop < 300
+      ) {
+        setNavbarColor("navbar-transparent");
+      }
+    };
+
+    window.addEventListener("scroll", updateNavbarColor);
+
+    return function cleanup() {
+      window.removeEventListener("scroll", updateNavbarColor);
+    };
+  });
+  return (
+    <Navbar
+      className={classnames("fixed-top", navbarColor)}
+      color-on-scroll="300"
+      expand="lg"
+    >
+      <Container>
+        <div className="navbar-translate">
+          <NavbarBrand
+            data-placement="bottom"
+            to="/index"
+            target="_blank"
+            title="Coded by Creative Tim"
+            tag={Link}
+          >
+            Star Wealth Assurance
+          </NavbarBrand>
+          <button
+            aria-expanded={navbarCollapse}
+            className={classnames("navbar-toggler navbar-toggler", {
+              toggled: navbarCollapse,
+            })}
+            onClick={toggleNavbarCollapse}
+          >
+            <span className="navbar-toggler-bar bar1" />
+            <span className="navbar-toggler-bar bar2" />
+            <span className="navbar-toggler-bar bar3" />
+          </button>
+        </div>
+        <Collapse
+          className="justify-content-end"
+          navbar
+          isOpen={navbarCollapse}
+        >
+          <Nav navbar>
+            <NavItem>
+              <NavLink to="/home" tag={Link}>
+                <i className="nc-icon nc-layout-11" /> Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+               to="/about-us" tag={Link}
+              >
+                <i className="nc-icon nc-book-bookmark" /> About Us
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/products" tag={Link}>
+                <i className="nc-icon nc-layout-11" /> Products
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/become-star-agent" tag={Link}>
+                <i className="nc-icon nc-layout-11" /> Become Star Agent
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/contact-us" tag={Link}>
+                <i className="nc-icon nc-layout-11" /> Contact Us
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/faq" tag={Link}>
+                <i className="nc-icon nc-layout-11" /> FAQ
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default ExamplesNavbar;
